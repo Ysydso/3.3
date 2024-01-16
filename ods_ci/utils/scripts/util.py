cd91e5cba6b00d3b56302c8345346e7160cda1b4
Ysydso <152947129+Ysydso@users.noreply.github.com>
2024-01-16 20:53:55 +0800

    cm 2

--- ods_ci/utils/scripts/util.py
+++ ods_ci/utils/scripts/util.py
@@ -113,7 +113,7 @@ def render_template(search_path, template_file, output_file, replace_vars):
 
     try:
         templateLoader = jinja2.FileSystemLoader(searchpath=search_path)
-        templateEnv = jinja2.Environment(loader=templateLoader, autoescape=False)  # Disable auto-escaping
+        templateEnv = jinja2.Environment(loader=templateLoader, autoescape=jinja2.select_autoescape(['html', 'xml']))  # Disable auto-escaping for HTML and XML
         template = templateEnv.get_template(template_file)
         outputText = template.render(replace_vars)
         with open(output_file, "w") as fh:
